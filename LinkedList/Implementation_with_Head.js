d5b90003ba6244dc343a0fdf3f9e5f12a66a45da
Mani <vsmaniraj1234@gmail.com>
2023-07-03 22:36:45 +0530

    Linked List Implementation with Head pointer alone

---
+++ LinkedList/Implementation_with_Head.js
@@ -0,0 +1,158 @@
+// Node class
+class Node {
+    constructor(val) {
+      this.val = val;
+      this.next = null;
+    }
+  }
+  
+  // Linked list Implementation with only head pointer
+  class LinkedList {
+    constructor() {
+      this.head = null;
+      this.size = 0;
+    }
+  
+    // O(1)
+    prepend(val) {
+      const node = new Node(val);
+      if (this.isEmpty()) {
+        this.head = node;
+      } else {
+        node.next = this.head;
+        this.head = node;
+      }
+      this.size++;
+    }
+  
+    // O(n)  --> can be optimisied to constant time O(1) with tail pointer
+    append(val) {
+      const node = new Node(val);
+      if (this.isEmpty()) {
+        this.head = node;
+      } else {
+        let currentNode = this.head;
+        while (currentNode.next) {
+          currentNode = currentNode.next;
+        }
+        currentNode.next = node;
+      }
+      this.size++;
+    }
+  
+    // Reuse prepend method if insert index = 0
+    // Reuse append method if insert index >= size of list
+    // Else traverse upto index-1 and add node & trailing portion
+    insert(index, val) {
+      if (index < 0 || index > this.size) return;
+  
+      if (index === 0) {
+        this.prepend(val);
+      } else if (index === this.size) {
+        this.append(val);
+      } else {
+        let currentNode = this.head;
+        for (let i = 0; i < index - 1; i++) {
+          currentNode = currentNode.next;
+        }
+        const node = new Node(val);
+        const temp = currentNode.next;
+        currentNode.next = node;
+        node.next = temp;
+        this.size++;
+      }
+    }
+    // Remove node at index, if found return value, else null
+    remove(index) {
+      if (index < 0 || index >= this.size || this.isEmpty()) return null;
+      let removedNode;
+      if (index === 0) {
+        removedNode = this.head;
+        this.head = this.head.next;
+      } else {
+        let currentNode = this.head;
+        for (let i = 0; i < index - 1; i++) {
+          currentNode = currentNode.next;
+        }
+        removedNode = currentNode.next;
+        currentNode.next = currentNode.next.next;
+      }
+      this.size--;
+      return removedNode.val;
+    }
+  
+    // Remove node based on value, if found return index, else -1
+    removeValue(val) {
+      if (this.isEmpty()) return -1;
+      if (this.head.val === val) {
+        this.head = this.head.next;
+        this.size--;
+        return 0;
+      }
+      let i = 1;
+      let currentNode = this.head;
+      while (currentNode.next && currentNode.next.val !== val) {
+        currentNode = currentNode.next;
+        i++;
+      }
+      if (currentNode.next) {
+        currentNode.next = currentNode.next.next;
+        this.size--;
+        return i;
+      }
+      return -1;
+    }
+  
+    // Search node, if found return index, else -1
+    search(val) {
+      if (this.isEmpty()) return -1;
+      if (this.head.val === val) {
+        return 0;
+      }
+      let i = 1;
+      let currentNode = this.head;
+      while (currentNode.next && currentNode.next.val !== val) {
+        currentNode = currentNode.next;
+        i++;
+      }
+      if (currentNode.next) {
+        return i;
+      }
+      return -1;
+    }
+  
+    print() {
+      if (this.isEmpty()) {
+        console.log("List is empty");
+        return;
+      }
+      let currentNode = this.head;
+      let values = [];
+      while (currentNode) {
+        values.push(currentNode.val);
+        currentNode = currentNode.next;
+      }
+      console.log(values.toString());
+    }
+  
+    isEmpty() {
+      return this.size === 0;
+    }
+  
+    getSize() {
+      return this.size;
+    }
+  }
+  
+  const list = new LinkedList();
+  list.prepend(5);
+  list.prepend(4);
+  list.append(6);
+  list.insert(2, 5.5);
+  console.log("removed val", list.remove(3));
+  list.append(7);
+  console.log("removed index", list.removeValue(7));
+  console.log("search", list.search(5));
+  list.print();
+  console.log(list);
+  
