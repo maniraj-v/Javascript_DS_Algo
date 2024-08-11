98c83f17b52c8bcd2198d065925bcbbc1f512b23
Mani <vsmaniraj1234@gmail.com>
2024-08-11 13:41:09 +0530

    Map filter reduce array poylfills

--- JS_Questions/Polyfills/Array.js
+++ JS_Questions/Polyfills/Array.js
@@ -20,3 +20,81 @@ Array.prototype.myFlat = function flatArray(level = 1) {
 
 console.log("flat", arrToBeFlat.flat(2));
 console.log("myflat", arrToBeFlat.myFlat(2));
+
+/* *********************************************************************** */
+/* Map
+/* *********************************************************************** */
+const numbers = [1, 2, 3, 4, 5];
+
+Array.prototype.myMap = function (callBack) {
+  const arr = this;
+  const result = [];
+  for (let i = 0; i < arr.length; i++) {
+    result.push(callBack(arr[i], i, arr));
+  }
+  return result;
+};
+
+console.log(
+  "map",
+  numbers.map((ele) => ele * 2)
+);
+console.log(
+  "myMap",
+  numbers.myMap((ele) => ele * 2)
+);
+
+/* *********************************************************************** */
+/* Filter
+/* *********************************************************************** */
+
+Array.prototype.myFilter = function (callBack) {
+  const arr = this;
+  const result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (callBack(arr[i], i, arr)) {
+      result.push(arr[i]);
+    }
+  }
+  return result;
+};
+
+console.log(
+  "filter",
+  numbers.filter((ele) => ele % 2 === 0)
+);
+console.log(
+  "myFilter",
+  numbers.myFilter((ele) => ele % 2 === 0)
+);
+
+/* *********************************************************************** */
+/* Reduce
+/* *********************************************************************** */
+
+Array.prototype.myReduce = function (callBack, intialValue) {
+  const arr = this;
+  let accumulator = intialValue;
+  let startIndex = 0;
+  if (intialValue === undefined) {
+    startIndex = 1;
+    accumulator = arr[0];
+  }
+  for (let i = startIndex; i < arr.length; i++) {
+    total = callBack(accumulator, arr[i], i, arr);
+  }
+  return total;
+};
+
+console.log(
+  "reduce",
+  numbers.reduce((accm, curr) => {
+    return accm + curr;
+  }, 0)
+);
+console.log(
+  "myReduce",
+  numbers.myReduce((accm, curr) => {
+    return accm + curr;
+  }, 10)
+);
