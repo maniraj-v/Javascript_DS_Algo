6242a5b9c8465642671f7c2e798d4241b2f980ea
maniraj-v <67851916+maniraj-v@users.noreply.github.com>
2025-01-02 15:58:21 +0530

    Update deepCopy.js

--- JS_Questions/Implementation/deepCopy.js
+++ JS_Questions/Implementation/deepCopy.js
@@ -1,13 +1,16 @@
 // Deep clone any variable type
-function deepClone(obj) {
-  if (obj && typeof obj === "object") {
-    const cloned = Array.isArray(obj) ? [] : {};
-    for (let key in obj) {
-      cloned[key] = deepClone(obj[key]);
-    }
-    return cloned;
+function deepCopy(value) {
+  if (Array.isArray(value)) {
+    return value.map((item) => deepCopy(item));
   }
-  return obj;
+  if (value !== null && typeof value === "object") {
+    const result = {};
+    for (const key in value) {
+      result[key] = deepCopy(value[key]);
+    }
+    return result;
+  }
+  return value;
 }
 
 const obj1 = {
@@ -20,6 +23,19 @@ const obj1 = {
     },
   },
 };
+
+//Another variation using for..in loop for both array and objects
+
+function deepClone(obj) {
+  if (obj && typeof obj === "object") {
+    const cloned = Array.isArray(obj) ? [] : {};
+    for (let key in obj) {
+      cloned[key] = deepClone(obj[key]);
+    }
+    return cloned;
+  }
+  return obj;
+}
 
 const newObj = deepClone(obj1);
 obj1.place.zip.second = "601"; // will not impact new object
