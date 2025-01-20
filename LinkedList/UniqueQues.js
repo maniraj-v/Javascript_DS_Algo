3d351e80aac0955eeb218b1d5c886e4832d25f3c
maniraj-v <67851916+maniraj-v@users.noreply.github.com>
2025-01-20 23:57:48 +0530

    Update UniqueQues.js

--- LinkedList/UniqueQues.js
+++ LinkedList/UniqueQues.js
@@ -127,3 +127,41 @@ var removeNodes = function(head) {
     }
     return result
 };
+
+/*
+
+Partition List
+Given the head of a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x.
+You should preserve the original relative order of the nodes in each of the two partitions.
+
+Input: head = [1,4,3,2,5,2], x = 3
+Output: [1,2,2,4,3,5]
+Example 2:
+
+Input: head = [2,1], x = 2
+Output: [1,2]
+
+*/
+
+var partition = function(head, x) {
+    let leftHead = new ListNode()
+    let left = leftHead
+    let rightHead = new ListNode()
+    let right = rightHead
+    let currentNode = head
+    while(currentNode){
+        const nextNodes = currentNode.next
+        currentNode.next = null
+        if(currentNode.val < x){
+            left.next = currentNode
+            left = left.next
+        }else{
+            right.next = currentNode
+            right = right.next
+        }
+        currentNode = nextNodes
+    }
+    left.next = rightHead.next
+    return leftHead.next
+};
+
