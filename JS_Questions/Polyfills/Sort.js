84ed1731af2913496c4c5e786c288c01d330db2f
maniraj-v <67851916+maniraj-v@users.noreply.github.com>
2025-05-06 22:32:04 +0530

    Update Sort.js

--- JS_Questions/Polyfills/Sort.js
+++ JS_Questions/Polyfills/Sort.js
@@ -18,46 +18,51 @@ console.log(array)
 // Merge Sort
 const array = [8, 12, 7, 1, 2, 6, 5]
 
-Array.prototype.mySort = function (compareFunction) {
-  compareFunction = compareFunction || ((a, b) => String(a) > String(b))
-
-  function mergeSort(arr) {
-    if (arr.length <= 1) {
-      return arr
-    }
-    const mid = Math.floor(arr.length / 2)
-    const left = arr.slice(0, mid)
-    const right = arr.slice(mid, arr.length)
-    const sortedLeft = mergeSort(left)
-    const sortedRight = mergeSort(right)
-    return merge(sortedLeft, sortedRight)
-  }
-
+Array.prototype.mySort = function (compareFn) {
   function merge(leftArr, rightArr) {
-    let i = 0; j = 0
-    const result = []
+    let i = 0;
+    let j = 0;
+    const result = [];
     while (i < leftArr.length && j < rightArr.length) {
-      if (compareFunction(leftArr[i], rightArr[j]) <= 0) {
-        result.push(leftArr[i])
-        i++
+      if (
+        compareFn
+          ? compareFn(leftArr[i], rightArr[j]) > 0
+          : String(leftArr[i]) > String(rightArr[j])
+      ) {
+        result.push(rightArr[j]);
+        j++;
       } else {
-        result.push(rightArr[j])
-        j++
+        result.push(leftArr[i]);
+        i++;
       }
     }
     while (i < leftArr.length) {
-      result.push(leftArr[i])
-      i++
+      result.push(leftArr[i]);
+      i++;
     }
     while (j < rightArr.length) {
-      result.push(rightArr[j])
-      j++
+      result.push(rightArr[j]);
+      j++;
     }
-    return result
+    return result;
   }
 
-  return mergeSort(this)
-
+  function mergeSort(arr) {
+    if (arr.length <= 1) {
+      return arr;
+    }
+    const mid = Math.floor(arr.length / 2);
+    const left = arr.slice(0, mid);
+    const right = arr.slice(mid, arr.length);
+    const sortedLeft = mergeSort(left);
+    const sortedRight = mergeSort(right);
+    return merge(sortedLeft, sortedRight);
+  }
+  const result = mergeSort(this);
+  for (let i = 0; i < this.length; i++) {
+    this[i] = result[i];
+  }
+  return this;
 };
 
 console.log(array.mySort()) // [1, 12, 2, 5, 6, 7, 8]
