f0c07613e7775e279b8e2f338346e5849ca9b8b0
maniraj-v <67851916+maniraj-v@users.noreply.github.com>
2025-05-07 16:23:38 +0530

    Update Function.js

--- JS_Questions/Polyfills/Function.js
+++ JS_Questions/Polyfills/Function.js
@@ -1,1 +1,26 @@
 // call, apply, bind
+
+
+const obj = {
+  firstName: "mani",
+  place: "chennai",
+}
+
+function greet(age) {
+  return this.firstName + " " + this.place + " " + age
+}
+
+// CALL Polyfill
+Function.prototype.myCall = function(ref,...props){
+		const func = this
+    const symbol = Symbol()
+    const obj = ref || globalThis
+    obj[symbol] = func
+       
+   const result= obj[symbol](...props)
+   delete obj[symbol]
+   return result
+}
+
+console.log(greet.myCall(obj, 23))
+console.log(greet.myCall(null, 23))
